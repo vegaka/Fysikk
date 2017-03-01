90f7dc83d770ef97054fd64fcecac67d3e33b551
Vegard Seim Karstang <vegard.karstang@gmail.com>
2017-03-01 14:33:16 +0100

    Added experimental data and rendering of the data.

--- project.py
+++ project.py
@@ -2,19 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import cos, sin, radians
 
-steps = 100
-step_size = 0.1
+steps = 100000
+step_size = 0.01
 
 # System variables
 g = 9.81
 alpha = radians(41)
-mu = 0.9
+mu = 62.083
 m = 0.2663
-k1 = 0.5
-k2 = 0.2
+k1 = 10.8
+k2 = 11.05
 
 
-def function(pos):
+def function(pos, vel):
     return g * (mu * cos(alpha) - sin(alpha)) - (1 / m) * (k1 * pos + k2 * (pos ** 3))
 
 
@@ -23,27 +23,27 @@ t_0 = 0
 v_0 = 0
 x_0 = 15.06
 
-time = np.zeros(steps + 1)
-position = np.zeros(steps + 1)
-velocity = np.zeros(steps + 1)
+time_length = 8
+h = time_length / steps
 
-time[0] = t_0
-position[0] = x_0
-velocity[0] = v_0
+time = np.linspace(0.8, time_length, steps)
+position = np.zeros(steps)
+velocity = np.zeros(steps)
 
-for n in range(steps):
-    # Euler's method
-    x_next = position[n] + step_size * velocity[n]
-    func_val = function(position[n])
-    v_next = velocity[n] + step_size * func_val
+for n in range(1, steps - 1):
+    velocity[n + 1] = h*function(position[n], velocity[n]) + velocity[n]
+    position[n + 1] = h*velocity[n] + position[n]
 
-    time[n + 1] = time[n] + step_size
-    position[n + 1] = x_next
-    velocity[n + 1] = v_next
+# Experimental data
+data = np.genfromtxt("data.txt", delimiter=",")
+timestamps = [point[0] for point in data]
+positions = [(point[1] - x_0) for point in data]
 
 plt.figure()
-plt.plot(time, position)
+plt.plot(time, position, label="Numerical")
+plt.plot(timestamps, positions, label="Experimental")
 plt.xlabel(r'$t$')
 plt.ylabel(r'$x(t)$')
+plt.legend(loc="lower right")
 plt.grid()
 plt.show()
